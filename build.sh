61504e010a2f3049ae25212413a0d9bb9de7c754
Paula <yosoypaulaandrea11@gmail.com>
2025-09-04 07:57:00 -0500

    ready for render.com

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/urs/bin/env bash
+# Exit on error
+set -o errexit
+
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+
+pip install -r requirements.txt
+
+# Collect static asset files
+python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
+python manage.py migrate
+
